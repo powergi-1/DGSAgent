8e6b1841e154e70a05718b0ad94cbb75e5c1cfb7
kwj <kwj@DESKTOP-GDV3M19>
2023-12-15 13:31:35 +0900

    first commit

---
+++ src/main/java/com/nnb/DSGAgent/ctrl/Service/CtrlService.java
@@ -0,0 +1,7 @@
+package com.nnb.DSGAgent.ctrl.Service;
+
+public interface CtrlService {
+	
+	public int startServer(CtrlVO startinfoVo) throws Exception;
+	public int stopServer(CtrlVO startinfoVo) throws Exception;
+}
